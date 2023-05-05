9ace3c1b905e2c9cf254e27ea2d190c9d7d6923d
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-05-05 15:56:19 +0200

    splice markup (index.php) and logic (functions.php)

---
+++ functions.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+
+Descrizione
+Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure. L’esercizio è suddiviso in varie milestone ed è molto importante svilupparle in modo ordinato.
+
+Milestone 1
+Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente. Scriviamo tutto (logica e layout) in un unico file index.php ✓
+
+Milestone 2
+Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale ✓
+
+Milestone 3 (BONUS)
+Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata che tramite $_SESSION recupererà la password da mostrare all’utente.
+
+Milestone 4 (BONUS)
+Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli. Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre insieme). Dare all’utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali.
+
+*/
+
+$password = '';
+
+if (!empty($_GET['passwordLength'])) {
+
+    $passwordLength = intval($_GET['passwordLength']);
+
+
+    function generatePassword($length)
+    {
+        $passwordRandom = '';
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"#$%&()*+,-./:;<=>?@[\]^_`{|}~';
+        for ($i = 0; $i < $length; $i++) {
+            $passwordRandom .= $characters[rand(0, strlen($characters) - 1)];
+        }
+        return $passwordRandom;
+    }
+
+    $password = generatePassword($passwordLength) . '@boolean.com';
+}
